46d91da8ef4e19059b2622820ad36e36f8b2df1d
Deepak Choudhary <97680719+Dgr81996@users.noreply.github.com>
2022-05-30 23:13:20 +0530

    Add files via upload

---
+++ rct-101-e2/src/App.js
@@ -0,0 +1,38 @@
+
+const container={ 
+
+
+  marginBottom: '50px',
+  marginLeft: '5%',
+  marginright:'5%',
+  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  function App() {
+    return (
+      <div>
+       <h2 style={container}>Products</h2> 
+      <h2>Count:1</h2>
+      <input type={"text"} placeholder={"select Products"}/>
+      
+      
+      <input type={"submit"}value={"Add Products"} />
+                  
+                 
+                      
+                  
+                 
+      </div>
+    );
+  }
+  
+  export default App;
+
+
